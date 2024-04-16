eec57dc5a70c73c2d15f5446626d5cc9f9c7a323
NathanGrima <nathangrima.m1@gmail.com>
2024-04-15 21:51:51 -0300

    alinhamento grafico

--- estatisticas.js
+++ estatisticas.js
@@ -42,7 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
         data: [65, 59, 80, 81, 56],
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
+        borderWidth: 1,
+
       }]
     };
   
