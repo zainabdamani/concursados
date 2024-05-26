b00b00edc4b15bde3ec3a669466728508a922544
zainabdamani <zaiinabg59@gmail.com>
2024-05-26 03:17:47 -0300

    salvar dados kanban localstorage

--- src/js/kanban.js
+++ src/js/kanban.js
@@ -1,3 +1,51 @@
+const addBtns = document.querySelectorAll(".add-task");
+
+function task(text) {
+
+return `
+<div class="item" draggable="true">${text}</div>
+`
+}
+
+addBtns.forEach((btn)=>{
+    btn.addEventListener("click", (e)=>{
+        const text = prompt("Escolha o nome do card");
+        const card = task(text);
+        const container = e.target.parentElement.parentElement;
+        container.insertAdjacentHTML("beforeend", card);
+        saveTask(container.id, text);
+        console.log(kanbanData)
+        drags();
+        
+    })
+})
+
+function saveTask(columnName, text){
+    const kanbanExists = kanbanData.some((kanban)=>{
+        return kanban.column===columnName
+    })
+    if (!kanbanExists){
+     kanbanData.push({
+        column: columnName,
+        cards: [
+             text
+        ]
+    })
+    return ;
+    }
+    let updatedKanbanData = kanbanData.map((kanban)=>{
+    
+        if (kanban.column===columnName){
+            kanban.cards.push(text)
+        } 
+        return kanban
+    })
+    kanbanData=updatedKanbanData;
+    localStorage.setItem("kanban", JSON.stringify(kanbanData))
+}
+
+
+function drags(){
 const items = document.querySelectorAll('.item')
 const columns = document.querySelectorAll('.column')
 let dragItem = null;
@@ -13,13 +61,16 @@ function dragStart() {
     setTimeout(() => this.className = 'invisible', 0)
 }
 function dragEnd() {
-    console.log('drag ended');
+    console.log('drag acabo');
       this.className = 'item'
       dragItem = null;
 }
 
 function dragDrop() {
     console.log('drag dropped');
+    if (dragItem===null){
+        return;
+    }
     this.append(dragItem);
 }
 
@@ -30,9 +81,6 @@ columns.forEach(column => {
     column.addEventListener('drop', dragDrop);
 });
 
-function dragOver() {
-    console.log('drag over');
-}
 function dragEnter() {
     console.log('drag entered');
 }
@@ -43,3 +91,23 @@ function dragOver(e) {
     e.preventDefault()
     console.log('drag over');
   }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    renderKanban();
+    drags();
+})
+function renderKanban () {
+    kanbanData.forEach((kanban)=>{
+        const column = document.querySelector(`.column#${kanban.column}`)
+        console.log(column)
+        
+        const cardlist = kanban.cards.map((item)=>{
+            return task(item)
+        }).join("")
+        console.log(cardlist)
+        column.insertAdjacentHTML("beforeend",cardlist)
+    })
+}
+
+let kanbanData = JSON.parse(localStorage.getItem("kanban")) ?? [];
