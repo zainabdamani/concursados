d85bd5f8bc02272a1cd95ed268b9cd9ad3dda765
Felipe <felipefalcao2000@hotmail.com>
2024-05-27 09:44:24 -0300

    incluindo a tela dark como tela inicial

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById("dark-mode-btn");
+    const darkModeButton = document.getElementById("dark-mode-btn");
     const currentMode = localStorage.getItem("dark-mode");
 
     if (currentMode === "enabled") {
         document.documentElement.classList.add("dark-mode");
     }
-    toggleButton.addEventListener("click", function() {
+    darkModeButton.addEventListener("click", function() {
         document.documentElement.classList.toggle("dark-mode");
         const mode = document.documentElement.classList.contains("dark-mode") ? "enabled" : "disabled";
         localStorage.setItem("dark-mode", mode);
     });
 });
+
+window.onload = function(){
+    currentMode(enabled);
+}
