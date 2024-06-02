8416c6f93cb66ccd6d502940e33f2c80ae9791dd
ferchinik <165728300+ferchinik@users.noreply.github.com>
2024-06-02 15:29:38 -0300

    Remoção Bootstrap

--- src/js/home.js
+++ src/js/home.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const comecarBtn = document.getElementById("comecarBtn");
-  
-    comecarBtn.addEventListener("click", function() {
-      const nome = document.getElementById("nome").value;
-      const sobrenome = document.getElementById("sobrenome").value;
-  
-      if (nome && sobrenome) {
-        // Armazenar nome e sobrenome no localStorage
-        localStorage.setItem("nome", nome);
-        localStorage.setItem("sobrenome", sobrenome);
-  
-        // Redirecionar para landing-page.html
-        window.location.href = "landing-page.html";
-      } else {
-        alert("Por favor, preencha todos os campos.");
-      }
-    });
+  const comecarBtn = document.getElementById("comecarBtn");
+
+  comecarBtn.addEventListener("click", function() {
+    const nome = document.getElementById("nome").value;
+    const sobrenome = document.getElementById("sobrenome").value;
+
+    if (nome && sobrenome) {
+
+      localStorage.setItem("nome", nome);
+      localStorage.setItem("sobrenome", sobrenome);
+      window.location.href = "landing-page.html";
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
   });
-  
-  
+});
