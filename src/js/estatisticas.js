a0b1b00c28c36492754cb7e9ede8d27cad6bc6e0
Nathan Grima <nathangrima.m1@gmail.com>
2024-06-04 14:11:40 -0300

    ajuste barra de tempo

--- src/js/estatisticas.js
+++ src/js/estatisticas.js
@@ -1,8 +1,8 @@
 function resumoDesempenhoMateria(Disciplina, totalPerguntas, percentualAcertos, tempoMedioResposta) {
-  this.Disciplina = Disciplina;
-  this.totalPerguntas = totalPerguntas;
-  this.percentualAcertos = percentualAcertos;
-  this.tempoMedioResposta = tempoMedioResposta;
+    this.Disciplina = Disciplina;
+    this.totalPerguntas = totalPerguntas;
+    this.percentualAcertos = percentualAcertos;
+    this.tempoMedioResposta = tempoMedioResposta;
 }
 
 let materiasResumo = new resumoDesempenhoMateria("Matematica", 120, 47, "1m27s");
@@ -12,43 +12,43 @@ let materiasResumo4 = new resumoDesempenhoMateria("Lingua Estrangeira", 120, 47,
 let materiasResumo5 = new resumoDesempenhoMateria("Geografia", 120, 47, "1m27s");
 
 function createHtmlCard(materia) {
-  const card = document.createElement("div");
-  card.classList.add("alinhar");
+    const card = document.createElement("div");
+    card.classList.add("alinhar");
 
-  const titulodiv = document.createElement("div");
-  titulodiv.classList.add("background-titulo");
-  const titulo = document.createElement("h3");
-  titulo.textContent = materia.Disciplina;
-  titulo.classList.add("background-titulo");
-  card.appendChild(titulo);
+    const titulodiv = document.createElement("div");
+    titulodiv.classList.add("background-titulo");
+    const titulo = document.createElement("h3");
+    titulo.textContent = materia.Disciplina;
+    titulo.classList.add("background-titulo");
+    card.appendChild(titulo);
 
-  const conteudo = document.createElement("div");
-  conteudo.classList.add("background-titulo");
-  
-  const totalPerguntas = document.createElement("p");
-  totalPerguntas.textContent = `Total de Perguntas: ${materia.totalPerguntas}`;
-  conteudo.appendChild(totalPerguntas);
+    const conteudo = document.createElement("div");
+    conteudo.classList.add("background-titulo");
 
-  const percentualAcertos = document.createElement("p");
-  percentualAcertos.textContent = `Percentual de Acertos: ${materia.percentualAcertos}%`;
-  conteudo.appendChild(percentualAcertos);
+    const totalPerguntas = document.createElement("p");
+    totalPerguntas.textContent = `Total de Perguntas: ${materia.totalPerguntas}`;
+    conteudo.appendChild(totalPerguntas);
 
-  const tempoMedioResposta = document.createElement("p");
-  tempoMedioResposta.textContent = `Tempo Médio de Resposta: ${materia.tempoMedioResposta}`;
-  conteudo.appendChild(tempoMedioResposta);
+    const percentualAcertos = document.createElement("p");
+    percentualAcertos.textContent = `Percentual de Acertos: ${materia.percentualAcertos}%`;
+    conteudo.appendChild(percentualAcertos);
 
-  card.appendChild(conteudo);
+    const tempoMedioResposta = document.createElement("p");
+    tempoMedioResposta.textContent = `Tempo Médio de Resposta: ${materia.tempoMedioResposta}`;
+    conteudo.appendChild(tempoMedioResposta);
 
-  return card;
+    card.appendChild(conteudo);
+
+    return card;
 }
 
 // Definição da função generateCardList
 function generateCardList(cards) {
-  const container = document.getElementById("product-container");
-  cards.forEach(materia => {
-      const card = createHtmlCard(materia);
-      container.appendChild(card);
-  });
+    const container = document.getElementById("product-container");
+    cards.forEach(materia => {
+        const card = createHtmlCard(materia);
+        container.appendChild(card);
+    });
 }
 
 // Criação da lista de matérias e geração dos cartões
@@ -65,66 +65,66 @@ localStorage.setItem("materias", JSON.stringify(materias));
 
 
 $(document).ready(function () {
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-      var target = $(e.target).attr("href"); // activated tab
-      $('.grafico').removeClass('active');
-      $(target).addClass('active');
-  });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href"); // activated tab
+        $('.grafico').removeClass('active');
+        $(target).addClass('active');
+    });
 
-  var ctxBar = document.getElementById('bar-chart').getContext('2d');
-  var ctxRadar = document.getElementById('radar-chart').getContext('2d');
+    var ctxBar = document.getElementById('bar-chart').getContext('2d');
+    var ctxRadar = document.getElementById('radar-chart').getContext('2d');
 
-  var barChart = new Chart(ctxBar, {
-      type: 'bar',
-      data: {
-          labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
-          datasets: [{
-              label: 'Desempenho',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: 'rgba(143, 0, 255, 0.2)',
-              borderColor: 'rgba(143, 0, 255, 1)',
-              borderWidth: 1
-          }]
-      },
-      options: {
-          animation: {
-              duration: 4000
-          },
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
+    var barChart = new Chart(ctxBar, {
+        type: 'bar',
+        data: {
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+            datasets: [{
+                label: 'Desempenho',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(143, 0, 255, 0.2)',
+                borderColor: 'rgba(143, 0, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            animation: {
+                duration: 4000
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 
-  var radarChart = new Chart(ctxRadar, {
-      type: 'radar',
-      data: {
-          labels: ['Matematica', 'Português', 'Conhecimentos Gerais', 'Informática', 'Lingua Estrangeira'],
-          datasets: [{
-              label: 'Desempenho',
-              data: [50, 100, 85, 30, 85],
-              backgroundColor: 'rgba(143, 0, 255, 0.2)',
-              borderColor: 'rgba(143, 0, 255, 1)',
-              borderWidth: 1
-          }]
-      },
-      options: {
-          animation: {
-              duration: 4000
-          },
-          scale: {
-              angleLines: {
-                  display: true
-              },
-              ticks: {
-                  suggestedMin: 0,
-                  suggestedMax: 100
-              }
-          }
-      }
-  });
+    var radarChart = new Chart(ctxRadar, {
+        type: 'radar',
+        data: {
+            labels: ['Matematica', 'Português', 'Conhecimentos Gerais', 'Informática', 'Lingua Estrangeira'],
+            datasets: [{
+                label: 'Desempenho',
+                data: [50, 100, 85, 30, 85],
+                backgroundColor: 'rgba(143, 0, 255, 0.2)',
+                borderColor: 'rgba(143, 0, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            animation: {
+                duration: 4000
+            },
+            scale: {
+                angleLines: {
+                    display: true
+                },
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }
+        }
+    });
 });
