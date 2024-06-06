44148309fa519acea4093f287e4f888ad4951abb
felipefalcao12 <felipefalcao2000@hotmail.com>
2024-06-06 20:19:45 -0300

    criacao de divs par a validacao das questoes

---
+++ src/js/questionary.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+const quiz = [
+  {
+    question: '(CESGRANRIO)- O Banco do Brasil é uma instituição financeira pública com capital misto, o que significa que:';
+    options:['Todas as ações do banco são de propriedade do governo.','Parte do capital do banco é privado e parte é do governo.','Todas as ações do banco são de propriedade de investidores privados.','O banco é controlado exclusivamente por instituições privadas.','O banco só pode operar no mercado interno.'],
+    answer:1
+  },
+  {
+    question: '(CESGRANRIO)- Sobre o mercado financeiro, é correto afirmar que:';
+    options:['O mercado de capitais é responsável pela negociação de títulos públicos.','O mercado monetário visa o controle da liquidez na economia.','O mercado de crédito é o responsável pela administração das reservas bancárias.','O mercado cambial envolve apenas a negociação de moedas nacionais.','O mercado de seguros é um segmento do mercado de crédito.'],
+    answer:1
+  },
+  {
+    question: '(CESGRANRIO)- O Banco do Brasil é uma instituição financeira pública com capital misto, o que significa que:';
+    options:['Todas as ações do banco são de propriedade do governo.','Parte do capital do banco é privado e parte é do governo.','Todas as ações do banco são de propriedade de investidores privados.','O banco é controlado exclusivamente por instituições privadas.','O banco só pode operar no mercado interno.'],
+    answer:1
+  },
+  {
+    question: '(CESGRANRIO)- Sobre o mercado financeiro, é correto afirmar que:';
+    options:['O mercado de capitais é responsável pela negociação de títulos públicos.','O mercado monetário visa o controle da liquidez na economia.','O mercado de crédito é o responsável pela administração das reservas bancárias.','O mercado cambial envolve apenas a negociação de moedas nacionais.','O mercado de seguros é um segmento do mercado de crédito.'],
+    answer:1
+  },
+  {
+    question: '(CESGRANRIO)- O Banco do Brasil é uma instituição financeira pública com capital misto, o que significa que:';
+    options:['Todas as ações do banco são de propriedade do governo.','Parte do capital do banco é privado e parte é do governo.','Todas as ações do banco são de propriedade de investidores privados.','O banco é controlado exclusivamente por instituições privadas.','O banco só pode operar no mercado interno.'],
+    answer:1
+  },
+  {
+    question: '(CESGRANRIO)- Sobre o mercado financeiro, é correto afirmar que:';
+    options:['O mercado de capitais é responsável pela negociação de títulos públicos.','O mercado monetário visa o controle da liquidez na economia.','O mercado de crédito é o responsável pela administração das reservas bancárias.','O mercado cambial envolve apenas a negociação de moedas nacionais.','O mercado de seguros é um segmento do mercado de crédito.'],
+    answer:1
+  }
+]
